b6246c7b0226e368b2ae7f1645e15a17e806d6b6
ootanl <6120nashville@gmail.com>
2024-01-17 09:33:53 +0900

    Initial Commit

---
+++ MonotoneCam/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MonotoneCam
+//
+//  Created by ootani yoshiyuki on 2024/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
